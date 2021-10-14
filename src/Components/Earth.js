b9d9da5db13be48bac541c49f49a3b1562258cf6
Shakiba448 <shakiba448@gmail.com>
2021-10-15 04:41:04 +0600

    WIP ON REACT 3D

--- src/Components/Earth.js
+++ src/Components/Earth.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const Earth = ({}) => {
+import EarthDayMap from "../assets/textures/8k_earth_daymap.jpg";
+import EarthNormalMap from "../assets/textures/8k_earth_normal_map.jpg";
+import EarthSpecularMap from "../assets/textures/8k_earth_specular_map.jpg";
+import EarthCloudMap from "../assets/textures/8k_earth_clouds.jpg";
+
+const Earth = () => {
   return (
     <>
+      <ambientLight intensity={1} />
       <mesh>
         <sphereBufferGeometry args={[1, 32, 32]} />
         <meshStandardMaterial color="red" />
