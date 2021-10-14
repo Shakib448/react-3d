67c01459c2e10685b448e247c2a011727f171dff
Shakiba448 <shakiba448@gmail.com>
2021-10-15 04:44:53 +0600

    WIP ON REACT 3D

--- src/Components/Earth.js
+++ src/Components/Earth.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 import EarthDayMap from "../assets/textures/8k_earth_daymap.jpg";
 import EarthNormalMap from "../assets/textures/8k_earth_normal_map.jpg";
@@ -6,6 +8,9 @@ import EarthSpecularMap from "../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudMap from "../assets/textures/8k_earth_clouds.jpg";
 
 const Earth = () => {
+  const [colorMap, normalMap, specularMap, cloudsMap] =
+    useLoader(TextureLoader,  [EarthDayMap, EarthNormalMap,EarthSpecularMap,EarthCloudMap]);
+
   return (
     <>
       <ambientLight intensity={1} />
