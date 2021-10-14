4e0aa136a7e6fd467fb4e9f9b752693ffcf8deec
Shakiba448 <shakiba448@gmail.com>
2021-10-15 04:22:11 +0600

    WIP ON REACT 3D

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Suspense } from "react";
 import "./App.scss";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Canvas>
+        <Suspense fallback={null}></Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
